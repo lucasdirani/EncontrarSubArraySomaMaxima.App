7b1a38e94667eeb15e590ae135d03eeb16acae02
Lucas Dirani <lruizd@protonmail.com>
2023-05-23 09:20:10 -0300

    Adicionar arquivos de projeto.

---
+++ Algoritmos/AlgoritmoSubArray.cs
@@ -0,0 +1,62 @@
+﻿namespace EncontrarSubArraySomaMaxima.App.Algoritmos
+{
+    public static class AlgoritmoSubArray
+    {
+        public static void EncontrarSubArrayDeSomaMaximaEmComplexidadeQuadratica(int[] arr, int n)
+        {
+            int indiceFinal = 0;
+            int maiorSomaResultado = arr[0];
+            for (int i = 0; i < n; i++) 
+            {
+                int maiorSomaAtual = 0;
+                for (int j = i; j < n; j++)
+                {
+                    maiorSomaAtual += arr[j];
+                    if (maiorSomaAtual > maiorSomaResultado)
+                    {
+                        maiorSomaResultado = maiorSomaAtual;
+                        indiceFinal = j;
+                    }
+                }
+            }
+            int indiceInicial = indiceFinal;
+            while (indiceInicial >= 0)
+            {
+                maiorSomaResultado -= arr[indiceInicial];
+                if (maiorSomaResultado == 0) break;
+                indiceInicial--;
+            }
+            for (int i = indiceInicial; i <= indiceFinal; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+
+        public static void EncontrarSubArrayDeSomaMaximaEmComplexidadeLinear(int[] arr, int n)
+        {
+            int indiceFinal = 0;
+            int maiorSomaAtual = arr[0];
+            int maiorSomaResultado = arr[0];
+            for (int i = 1; i < n; i++) 
+            {
+                maiorSomaAtual = arr[i] > (arr[i] + maiorSomaAtual) ? arr[i] : (arr[i] + maiorSomaAtual);
+                if (maiorSomaAtual > maiorSomaResultado)
+                {
+                    maiorSomaResultado = maiorSomaAtual;
+                    indiceFinal = i;
+                }
+            }
+            int indiceInicial = indiceFinal;
+            while (indiceInicial >= 0)
+            {
+                maiorSomaResultado -= arr[indiceInicial];
+                if (maiorSomaResultado == 0) break;
+                indiceInicial--;
+            }
+            for (int i = indiceInicial; i <= indiceFinal; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using EncontrarSubArraySomaMaxima.App.Performance;
+
+_ = BenchmarkRunner.Run<TestePerformanceAlgoritmoSubArray>();
